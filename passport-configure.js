d96ba2a1775a427951da0d057ef3a34dfc9640a4
GauravRasal07 <gauravvr77@gmail.com>
2021-04-20 22:13:22 +0530

    changed icons

--- passport-configure.js
+++ passport-configure.js
@@ -3,9 +3,9 @@ const passport       = require('passport'),
 	    User           = require("./models/user");
 
 passport.use(new GoogleStrategy({
-    clientID: '917632693332-g5sv7h0eas1ol8c5u24h5v31g4m4v0bj.apps.googleusercontent.com',
-    clientSecret: 'iO6dfScHHIwlEQHrrbmlfUSH',
-    callbackURL: "http://localhost:3000/auth/google/callback"
+    clientID: process.env.clientID,
+    clientSecret: process.env.clientSecret,
+    callbackURL: "https://the-news-app-gr.herokuapp.com/auth/google/callback"
   },
   (accessToken, refreshToken, profile, email, done) => {
     User.findOne({username : email.emails[0].value}, (err, user) => {
